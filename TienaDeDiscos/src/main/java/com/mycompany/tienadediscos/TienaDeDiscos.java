d5210548b6c0bdf8fc7e1725302ca8072584afab
Didier <dbolivar60804@ufide.ac.cr>
2024-10-08 22:13:40 -0600

    intento de adaptación

---
+++ TienaDeDiscos/src/main/java/com/mycompany/tienadediscos/TienaDeDiscos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tienadediscos;
+
+/**
+ *
+ * @author rdcd2
+ */
+public class TienaDeDiscos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ TiendaDeDiscos/src/test/java/TiendaDeDiscos/demo/TiendaDeDiscosApplicationTests.java
@@ -0,0 +1,13 @@
+package TiendaDeDiscos.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaDeDiscosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
